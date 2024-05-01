b99c73cdee4d543eddb1b9a82a90a63c1778e005
diom1o <161154194+diom1o@users.noreply.github.com>
2024-05-01 12:04:11 +0200

    advance the readability

--- config.js
+++ config.js
@@ -1,20 +1,12 @@
 require('dotenv').config();
-const config = {
-  common: {
-    PORT: process.env.PORT || 3000,
-  },
-  development: {
-    DATABASE_URL: process.env.DEVELOPMENT_DATABASE_URL,
-    API_KEY: process.env.DEVELOPMENT_API_KEY,
-  },
-  production: {
-    DATABASE_URL: process.env.PRODUCTION_DATABASE_URL,
-    API_KEY: process.env.PRODUCTION_API_KEY,
-  }
-};
-const ENVIRONMENT = process.env.NODE_ENV || 'development';
-const finalConfig = {
-  ...config.common,
-  ...(config[ENVIRONMENT] || {}),
-};
+
+const getConfig = (env) => ({
+  PORT: process.env.PORT || 3000,
+  DATABASE_URL: process.env[`${env}_DATABASE_URL`],
+  API_KEY: process.env[`${env}_API_KEY`],
+});
+
+const ENVIRONMENT = process.env.NODE_ENV ? process.env.NODE_ENV.toUpperCase() : 'DEVELOPMENT';
+const finalConfig = getConfig(ENVIRONMENT);
+
 module.exports = finalConfig;
